fbd5378c9ac94a4acff74b5721593849980cb33e
James Lee <james@jameslee.ca>
2014-01-22 16:02:05 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestSpreeStoreCredit::Application.config.secret_key_base = '23b73e43da8474297bebe1f927b21e0150f5b281e3ceb97b9fc06fc7ea4af525456621ad052b9912f6743ef12b79d7d9468c6939fcfb02060804c39959111f49'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestSpreeStoreCredit::Application.config.session_store :cookie_store, key: '_test_spree_store_credit_session'
